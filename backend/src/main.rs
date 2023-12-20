ac3819177972cbdbd2bcae4526cab9d97fe3a835
Aejkatappaja <joverjlj@gmail.com>
2023-12-20 13:36:24 +0100

    wip(main.rs): structuring logic

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,5 @@
-use postgres::Error as PostgresError;
 use postgres::{Client, NoTls};
+use postgres::{Error as PostgresError, GenericClient};
 use std::env;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
@@ -22,6 +22,122 @@ const OK_RESPONSE: &str =
     "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n";
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(_) = set_database() {
+        print!("Error setting database");
+        return;
+    }
+
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    print!("🚀 Server listening on port 8080 !");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => handle_client(stream),
+            Err(e) => {
+                print!("unable to connect: {}", e)
+            }
+        }
+    }
+}
+
+//db setup
+fn set_database() -> Result<(), PostgresError> {
+    let mut client = Client::connect(DB_URL, NoTls)?;
+    client.batch_execute(
+        "
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIALS PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        email VARCHAR NOT NULL
+    )
+    ",
+    )?;
+    Ok(())
+}
+
+// get id from request url
+fn get_id(request: &str) -> &str {
+    request
+        .split("/")
+        .nth(4)
+        .unwrap_or_default()
+        .split_whitespace()
+        .next()
+        .unwrap_or_default()
+}
+
+// deserialize user from request body without id
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
+}
+
+//handle requests
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut request = String::new();
+
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
+
+            let (status_line, content) = match &*request {
+                r if r.starts_with("OPTIONS") => (OK_RESPONSE.to_string(), "".to_string()),
+                r if r.starts_with("POST /api/rust/users") => handle_post_request(r),
+                // r if r.starts_with("GET /api/rust/users") => handle_get_request(r),
+                // r if r.starts_with("GET /api/rust/users") => handle_get_all_request(r),
+                // r if r.starts_with("PUT /api/rust/users") => handle_put_request(r),
+                // r if r.starts_with("DELETE /api/rust/users") => handle_post_request(r),
+                _ => (NOT_FOUND.to_string(), "404 not found".to_string()),
+            };
+
+            stream
+                .write_all(format!("{}{}", status_line, content).as_bytes())
+                .unwrap();
+        }
+        Err(e) => eprintln!("unable to read stream: {}", e),
+    }
+}
+
+fn handle_post_request(request: &str) -> (String, String) {
+    match (
+        get_user_request_body(request),
+        Client::connect(DB_URL, NoTls),
+    ) {
+        (Ok(user), Ok(mut client)) => {
+            // Insert the user and retrieve the ID
+            let row = client
+                .query_one(
+                    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+                    &[&user.name, &user.email],
+                )
+                .unwrap();
+
+            let user_id: i32 = row.get(0);
+            //fetch the created user data
+            match client.query_one(
+                "SELECT id, name, email FROM users WHERE id = $1",
+                &[&user_id],
+            ) {
+                Ok(row) => {
+                    let user = User {
+                        id: Some(row.get(0)),
+                        name: row.get(1),
+                        email: row.get(2),
+                    };
+
+                    (
+                        OK_RESPONSE.to_string(),
+                        serde_json::to_string(&user).unwrap(),
+                    )
+                }
+                Err(_) => (
+                    INTERNAL_ERROR.to_string(),
+                    "Failed to retrieve created user".to_string(),
+                ),
+            }
+        }
+        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
+    }
 }
