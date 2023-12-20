dc3ce6240c03feb525f59e280c34220fba297097
Aejkatappaja <joverjlj@gmail.com>
2023-12-20 11:31:06 +0100

    init(backend): main.rs

---
+++ backend/src/main.rs
@@ -0,0 +1,27 @@
+use postgres::Error as PostgresError;
+use postgres::{Client, NoTls};
+use std::env;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
+
+//DATABASE URL
+const DB_URL: &str = env!("DATABASE_URL");
+
+//constants
+const OK_RESPONSE: &str =
+    "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
+fn main() {
+    println!("Hello, world!");
+}
