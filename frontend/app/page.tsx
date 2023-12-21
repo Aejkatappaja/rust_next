ee7b237acfa8316bd267012c7fa644d9ba0e3b44
Aejkatappaja <joverjlj@gmail.com>
2023-12-21 18:50:46 +0100

    test{page}: style

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,28 +16,59 @@ export default async function Home() {
   // console.log(data, 'data');
 
   return (
-    <main className='flex flex-col items-center gap-8 p-6 md:p-8'>
-      <div className='flex w-full justify-between  py-4 md:justify-center md:gap-10'>
+    <main className='flex flex-col items-center gap-8 md:px-12'>
+      <div className='flex w-full justify-between p-6 py-4 md:justify-center md:gap-10 md:p-8'>
         <HeaderImages />
       </div>
       <h1 className='text-gradient font-mono text-3xl font-semibold tracking-wider md:text-5xl'>
-        {strings.main_title}
+        🦀 {strings.main_title} 🦀
       </h1>
-      <div className='grid w-full grid-cols-1 gap-4 border py-2 md:px-56 2xl:px-96'>
-        <section className='h-52 rounded-2xl border px-4 py-2'>
-          <h1>Users List</h1>
-
-          {data?.map((item) => {
-            return (
-              <div key={item.name}>
-                {item.id} {item.name} {item.email}
-              </div>
-            );
-          })}
+      <div className='bg-rainbow grid w-full grid-cols-1 gap-4 rounded-2xl px-4 py-4 md:px-56 2xl:px-96'>
+        <section className='flex flex-col items-center justify-start gap-2 overflow-y-scroll rounded-2xl bg-zinc-900 px-4 py-2 shadow-lg shadow-black'>
+          <h1 className='text-xl font-semibold'>{strings.users.title}</h1>
+          <div className='flex w-full flex-col gap-4 py-4'>
+            {data?.map((item) => {
+              const { id, name, email } = item;
+              return (
+                <div
+                  key={item.name}
+                  className='flex flex-col gap-2 rounded-lg border px-4 py-4 md:flex-row'
+                >
+                  <label htmlFor='id' className='text-lg'>
+                    id:{' '}
+                    <input
+                      name='id'
+                      type='text'
+                      value={id}
+                      disabled
+                      className='w-8 rounded-lg  border text-center'
+                    />{' '}
+                  </label>
+                  {/* <label htmlFor='name' className='text-lg'>
+                    name:{' '}
+                    <input
+                      name='name'
+                      type='text'
+                      value={name}
+                      disabled
+                      className='rounded-lg  border text-center'
+                    />{' '}
+                  </label>
+                  <label htmlFor='email' className='text-lg'>
+                    email:{' '}
+                    <input
+                      name='email'
+                      type='email'
+                      value={email}
+                      disabled
+                      className='rounded-lg  border text-center'
+                    />{' '}
+                  </label> */}
+                </div>
+              );
+            })}
+          </div>
         </section>
-        {/* <section className='h-52 rounded-2xl border'>{user?.name}</section>
-        <section className='h-52 rounded-2xl border'></section>
-        <section className='h-52 rounded-2xl border'></section> */}
       </div>
     </main>
   );
