3bff22c84dc6d69c7220ef18934d467f7d885d27
Aejkatappaja <joverjlj@gmail.com>
2023-12-21 18:49:44 +0100

    delete(utils): getColorClass issues

--- frontend/components/ImageLink.tsx
+++ frontend/components/ImageLink.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { getColorClass } from '@/utils/get-color-class';
 
 interface ImageLinkProps {
   href: string;
@@ -11,18 +10,17 @@ interface ImageLinkProps {
 }
 
 export const ImageLink: React.FC<ImageLinkProps> = ({ ...props }) => {
-  const color = getColorClass(props.name);
   return (
     <Link
       href={props.href}
       target='_blank'
-      className={`relative h-24 w-24 rounded-full border-2 border-transparent bg-white duration-500 hover:-translate-y-2 hover:shadow-lg hover:border-${color} hover:shadow-${color}  md:h-40 md:w-40`}
+      className='md:h-30 md:w-30 relative h-24 w-24 rounded-full border-2 border-transparent bg-white duration-500 hover:-translate-y-2 hover:shadow-lg'
     >
       <Image
         src={props.src}
         fill
         alt={props.alt}
-        className='rounded-full object-contain object-center '
+        className='rounded-full object-contain object-center'
       />
     </Link>
   );
