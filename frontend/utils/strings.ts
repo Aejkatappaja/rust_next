71d3c8726c300ca7c995bc1faff533018a70b42f
Aejkatappaja <joverjlj@gmail.com>
2023-12-21 16:52:10 +0100

    add(utils_strings): links typo content

--- frontend/utils/strings.ts
+++ frontend/utils/strings.ts
@@ -1,3 +1,23 @@
 export const strings = {
   main_title: 'RUST CRUD',
+  links: {
+    rust: {
+      name: 'rust',
+      alt: 'rust-crab-logo',
+      href: 'https://www.rust-lang.org/',
+      src: '/images/rust_crab.png',
+    },
+    nextjs: {
+      name: 'nextjs',
+      alt: 'nextjs-logo',
+      href: 'https://nextjs.org/',
+      src: '/images/next_js.webp',
+    },
+    docker: {
+      name: 'docker',
+      alt: 'docker-logo',
+      href: 'https://www.docker.com/',
+      src: '/images/docker.jpg',
+    },
+  },
 };
