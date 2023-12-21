af3f59e19e3f454c8aeda05a960fdc0f423a9ddb
Aejkatappaja <joverjlj@gmail.com>
2023-12-21 13:53:08 +0100

    add(utils): links

---
+++ frontend/utils/links.ts
@@ -0,0 +1,22 @@
+import { ImageLink } from '../components/ImageLink';
+
+export const links = [
+  {
+    alt: 'rust-crab-logo',
+    href: 'https://www.rust-lang.org/',
+    src: '/images/rust_crab.png',
+    color: 'orange-500',
+  },
+  {
+    alt: 'nextjs-logo',
+    href: 'https://nextjs.org/',
+    src: '/images/next_js.webp',
+    color: 'violet-500',
+  },
+  {
+    alt: 'https://www.docker.com/',
+    href: 'https://www.rust-lang.org/',
+    src: '/images/docker.jpg',
+    color: 'blue-400',
+  },
+];
