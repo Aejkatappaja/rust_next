851b2e087a17f77ac511efb242a2a4608aa19af0
Aejkatappaja <joverjlj@gmail.com>
2023-12-21 16:54:28 +0100

    test(page): ui rendering

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import deleteUser from '@/actions/deleteUser';
 import getUserById from '@/actions/getUserById';
 import getUsers from '@/actions/getUsers';
 import { HeaderImages } from '@/components/HeaderImages';
@@ -9,25 +10,28 @@ export default async function Home() {
   // const user = await getUserById(1);
   // console.log(user, 'user');
 
-  console.log(data, 'data');
+  // const deletePpl = await deleteUser();
+  // console.log(deletePpl);
+
+  // console.log(data, 'data');
 
   return (
     <main className='flex flex-col items-center gap-8 p-6 md:p-8'>
       <div className='flex w-full justify-between  py-4 md:justify-center md:gap-10'>
         <HeaderImages />
       </div>
-      <h1 className='bg-gradient-to-r from-fuchsia-500 to-orange-500 bg-clip-text font-mono text-3xl font-semibold tracking-wider text-transparent md:text-5xl'>
+      <h1 className='text-gradient font-mono text-3xl font-semibold tracking-wider md:text-5xl'>
         {strings.main_title}
       </h1>
-      <div className='grid w-full gap-4 px-2 py-2 md:grid-cols-2'>
+      <div className='grid w-full grid-cols-1 gap-4 border py-2 md:px-56 2xl:px-96'>
         <section className='h-52 rounded-2xl border px-4 py-2'>
           <h1>Users List</h1>
 
           {data?.map((item) => {
             return (
-              <p key={item.name}>
-                {item.name} {item.email}
-              </p>
+              <div key={item.name}>
+                {item.id} {item.name} {item.email}
+              </div>
             );
           })}
         </section>
