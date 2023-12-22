679d0c45d37ee874f8ebdac014af8767b0e0b1ca
Aejkatappaja <joverjlj@gmail.com>
2023-12-22 12:46:24 +0100

    add(components): DeleteButton

---
+++ frontend/components/Button.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import deleteUser from '@/actions/deleteUser';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { GiCrossedClaws } from 'react-icons/gi';
+
+interface ButtonProps {
+  id: number;
+}
+
+export const Button: React.FC<ButtonProps> = ({ id }) => {
+  const router = useRouter();
+
+  const handleDeleteUser = async (id: number) => {
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <button className='' onClick={() => handleDeleteUser(id)}>
+      <GiCrossedClaws className=' rounded-full text-orange-600' />
+    </button>
+  );
+};
